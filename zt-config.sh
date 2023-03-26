7007648736af46f967476fc20381a7830d721e50
Franciszek Dec <f.dec@stud.elka.pw.edu.pl>
2023-03-26 15:52:08 +0200

    ✨ ZeroTier config script

---
+++ zt-config.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied. Provide 2 - LAN interface name and ZT interface name."
+    exit
+fi
+
+if [ $# -eq 1 ]
+  then
+    echo "Please provide one more argument. 1 - LAN interface, 2 - ZT interface"
+    exit
+fi
+
+if [ $# -eq 2 ]
+  then
+
+    echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+    sysctl -p
+
+    iptables -t nat -A POSTROUTING -o $1 -j MASQUERADE
+    iptables -A FORWARD -i $1 -o $2 -m state --state RELATED,ESTABLISHED -j ACCEPT
+    iptables -A FORWARD -i $2 -o $1 -j ACCEPT
+    echo "OK"
+fi
