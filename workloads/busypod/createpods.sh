c7a715fdfd552bbdff703b617a033d571a288415
Dariusz Bursztynowski <dariusz.bursztynowski@orange.com>
2024-06-03 12:12:57 +0200

    Update createpods.sh

--- workloads/busypod/createpods.sh
+++ workloads/busypod/createpods.sh
@@ -4,36 +4,41 @@
 # For details about parametrizing the pod refer to the Docker Hub repo: 
 #   https://hub.docker.com/repository/docker/dburszty/artificial-workload-amd64/general
 
+# Number of pods to be created; notice one can modify the while loop to generate an "infinite" number of pods.
 PODS_TO_CREATE=1
+
+# Namespace to be created
 NAMESPACE="congestion"
+
+# Pod's name prefix
 PODPREFIX="congest"
+
+# Choose appropriate architecture: amd64 or arm64v8
 IMAGE="dburszty/artificial-workload-arm64v8:latest"
 
-kubectl create namespace $NAMESPACE > /dev/null 2>&1
-#kubectl create namespace $NAMESPACE
+kubectl create namespace $NAMESPACE > /dev/null 2>&1    # > /dev/null 2>&1   - ignores command output
 
 i=0
 cont=true
-
 while $cont
 do
 
-  # pod name will contain: a fixed prefix - consecutive integer number - unigue suffix expressing milliseconds elapsed from Epoch
+  # pod name will contain: <fixed prefix>-<consecutive integer number>-<unigue suffix expressing milliseconds elapsed from Epoch>
   i=$((i+1))
   name="$PODPREFIX-$i-"$(date +%s%3N)
   echo $name
   
-#  examples 
-#  kubectl run -n $NAMESPACE $name --image=$IMAGE --restart=Never -- 10000 5 oiter=1
-#  with default values: kubectl run -n $NAMESPACE $name --image=$IMAGE --restart=Never
-#  with run time equal ~30 sec: 
+  # examples 
+  #kubectl run -n $NAMESPACE $name --image=$IMAGE --restart=Never -- 10000 5 oiter=1
+  #with default values: kubectl run -n $NAMESPACE $name --image=$IMAGE --restart=Never
+  #with run time equal ~30 sec: 
   kubectl run -n $NAMESPACE $name --image=$IMAGE --restart=Never -- 10000 5 otime=30
   
-  # we set sleeptime = 2.5 + random[0 ... 1] sec.
+  # we set sleeptime = 2.5 + random-number[0 ... 1] seconds
   base=2.5
   denom=32767                # because $RAND in [0..32767]
-  sleeptime=$(echo "scale=4; $base + $RANDOM / $denom" | bc)   # use bc - basic calculator utility for floating
-  echo "sleep = $sleeptime"
+  sleeptime=$(echo "scale=4; $base + $RANDOM / $denom" | bc)   # use bc - basic calculator utility for floating point
+  #echo "sleep = $sleeptime"
   sleep $sleeptime
 
   if [ $i == $PODS_TO_CREATE ]; then
