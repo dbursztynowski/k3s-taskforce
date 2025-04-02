5e34a172c336cdb29ca2cacef5e53329e1642143
ubuntu <ubuntu@labs.myguest.virtualbox.org>
2025-04-02 21:32:58 +0200

    update shutcluster.sh script

--- shutcluster.sh
+++ shutcluster.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-cd pi-cluster-install
-./shutcluster.sh
+#cd pi-cluster-install
+#./pi-cluster-install/shutcluster.sh
+CLUSTER_INV="./pi-cluster-install/inventory/hosts.ini"
+ansible all -i $CLUSTER_INV -b -B 1 -P 0 -m shell -a "sleep 5 && shutdown now" -b
 
 # temperature reading cmd
 # vcgencmd measure_temp
