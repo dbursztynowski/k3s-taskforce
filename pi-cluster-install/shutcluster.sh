c812cafb862a97d7e269a8d647dddf6a4ba068a2
Dariusz Bursztynowski <dariusz.bursztynowski@pw.edu.pl>
2025-12-10 17:36:37 +0100

    Update shutcluster.sh

--- pi-cluster-install/shutcluster.sh
+++ pi-cluster-install/shutcluster.sh
@@ -2,6 +2,7 @@
 # check the following links for the ansible command options used:
 # https://www.jeffgeerling.com/blog/2018/reboot-and-wait-reboot-complete-ansible-playbook
 # https://www.middlewareinventory.com/blog/ansible_wait_for_reboot_to_complete/
+# https://docs.ansible.com/projects/ansible/latest/playbook_guide/playbooks_async.html#asynchronous-ad-hoc-tasks
 #CLUSTER_INV="shutdown-hosts.ini"
 CLUSTER_INV="inventory/hosts.ini"
-ansible all -i $CLUSTER_INV -b -B 1 -P 0 -m shell -a "sleep 5 && shutdown now" -b
+ansible all -i $CLUSTER_INV -B 1 -P 0 -m shell -a "sleep 5 && shutdown now" --become
