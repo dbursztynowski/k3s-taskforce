7a3a802dad092587c5e63ba880762e497f3eee87
Dariusz Bursztynowski <dariusz.bursztynowski@pw.edu.pl>
2026-05-08 12:55:34 +0200

    Update shutcluster.sh

--- shutcluster.sh
+++ shutcluster.sh
@@ -4,7 +4,8 @@
 CLUSTER_INV="./pi-cluster-install/inventory/hosts.ini"
 ansible all -i $CLUSTER_INV -b -B 1 -P 0 -m shell -a "sleep 5 && shutdown now" -b
 
-# temperature reading cmd
+# Run on Raspberry Pi to read CPU temperature
+# use one of two options:
 # vcgencmd measure_temp
 # or
 # cat /sys/class/thermal/thermal_zone0/temp
